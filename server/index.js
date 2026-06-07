a8c644fab60122335c9ff21f3c648785cb98c5f0
Shrutika Shaw <shrutika.shaw2015@gmail.com>
2026-06-07 21:53:54 +0530

    adding health route

--- server/index.js
+++ server/index.js
@@ -14,5 +14,9 @@ app.use('/api/users', require('./routes/users'))
 app.use('/api/projects', require('./routes/projects'))
 app.use('/api/tasks', require('./routes/tasks'))
 
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`TaskFlow server running on http://localhost:${PORT}`))
